51b362c14813bbb7bc21fcce685d6c4be1ae3b41
shruti patel <Shrutipatel203@gmail.com>
2023-11-15 22:02:23 +0000

    mycommit

---
+++ Constroctorclass.java
@@ -0,0 +1,16 @@
+package constructorparameter;
+
+public class Constroctorclass {
+    int modelyear;
+    String modelname;
+
+
+    Constroctorclass(int year, String name){
+        modelname = year;
+        modelyear = name;
+
+    }
+
+
+
+}
